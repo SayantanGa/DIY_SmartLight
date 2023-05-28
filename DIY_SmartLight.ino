e8e68bab99dbb6342a04a2015b60c6ee5f716ebe
SayantanGa <103838076+SayantanGa@users.noreply.github.com>
2023-05-28 22:33:45 +0530

    Update DIY_SmartLight.ino

--- DIY_SmartLight.ino
+++ DIY_SmartLight.ino
@@ -1,51 +1,52 @@
-#define LDR_PIN A1
+#include <Wire.h>
+#include <BH1750.h>
+#include <SoftwareSerial.h>
+#include <math.h>
 
-const int DEFAULT_BRIGHTNESS = 196;
-const int DEFAULT_TEMP_INDEX = 2;
-int LDR = 0;
+BH1750 lightInput;
+const int DEFAULT_BRIGHTNESS = 196;  //Default brightness while LED starting
+const int DEFAULT_TEMP_INDEX = 2;  //Default functuion to use for processing rgb temperature
+float LUX = 0.0;  //Initializing BH1750 input float variable to 0.0
 
-//Function definitions for controlling brightness:
-int LinearBrightness(int x){
-    return int(255*(1-x/1023));
+/*Function definitions for controlling brightness*/
+
+int ConstantBrightness(int x){  //For constant brightness output
+	return 255*x/9.0;
 }
 
-int HigherBrightness(int x){
-    return int(255*(sqrt(1-(x*x)/1046529)));
-}
-
-int LowerBrightness(int x){
-    return int(255*(1-(sqrt(1-(1-x/1023)*(1-x/1023)))));
+int LogBrightness(float x){
+  return int((100.589-19.811*log(x+1.5))*2.55);
 }
 
 //
-int Temperatures[11][3] = {{200,245,254},{240,255,255},{255,255,255},{255,255,200},{248,255,183},{255,248,167},{255,234,144},{255,218,122},{255,182,78},{255,139,39},{255,89,11}};
-int (*Brightness_Functions[3])(int) = {LinearBrightness, HigherBrightness, LowerBrightness};
+int Temperatures[11][3] = {{200,245,254},{240,255,255},{255,255,255},{255,255,200},{248,255,183},{255,248,167},{255,234,144},{255,218,122},{255,182,78},{255,139,39},{255,89,11}};  //rgb values for specific temperatures 
+int (*Brightness_Functions[2])(float) = {LogBrightness, ConstantBrightness};  //array of pointers to brightness functions
 
-class LED{
+class LED{  //class to store the variables and functions corresponding to the operating light
     public:
-        int Pins[2] = {3, 5};
-        int RGB_Pins[3] = {9, 10, 11};
+        int Pins[2] = {3, 5};  //Pins controlling SMD LEDs
+        int RGB_Pins[3] = {9, 10, 11};  //Pins controlling the r, g and b values of a RGB LED respectively
         int brightness = DEFAULT_BRIGHTNESS;
-        int brightness_function_index = 0;
-        int temp_index = DEFAULT_TEMP_INDEX;
-        int r = Temperatures[temp_index][0]*(brightness/255);
-        int g = Temperatures[temp_index][1]*(brightness/255);
-        int b = Temperatures[temp_index][2]*(brightness/255);
+        int brightness_function_index = 0;  //Index of the brightness function in use
+        int temp_index = DEFAULT_TEMP_INDEX;  //Current operating index of Temperatures array
+        int r = Temperatures[temp_index][0]*(brightness/255.0);
+        int g = Temperatures[temp_index][1]*(brightness/255.0);
+        int b = Temperatures[temp_index][2]*(brightness/255.0);
   
-        void glow(){
+        void glow(){  //Glows the Light at set brightness and temperature
             for (int i = 0; i < 2; i++){
                 analogWrite(Pins[i], brightness);
               	delay(50);
-              	analogWrite(RGB_Pins[0], r);
+			      }
+            analogWrite(RGB_Pins[0], r);
         		delay(50);
         		analogWrite(RGB_Pins[1], g);
         		delay(50);
         		analogWrite(RGB_Pins[2], b);
         		delay(50);
-            }
         }
   
-        void dim(){
+        void dim(){  //Turns off the Light
             for (int i = 0; i < 2; i++){
                 digitalWrite(Pins[i], LOW);
               	delay(50);
@@ -56,81 +57,79 @@ class LED{
             }
         }
   
-        void update(){
-            brightness = Brightness_Functions[brightness_function_index](LDR);
-            r = Temperatures[temp_index][0]*(brightness/255);
-            g = Temperatures[temp_index][1]*(brightness/255);
-            b = Temperatures[temp_index][2]*(brightness/255);
+        void update(){  //Updates the Light brightness and temperature according to LUX and User input
+            brightness = (*Brightness_Functions[0])(LUX/32.0);
+            r = Temperatures[temp_index][0]*(brightness/255.0);
+            g = Temperatures[temp_index][1]*(brightness/255.0);
+            b = Temperatures[temp_index][2]*(brightness/255.0);
             glow();
         }
+		
+		    void update_with_const_brightness(int level){
+			      brightness_function_index = 1;
+		  	    brightness = (*Brightness_Functions[brightness_function_index])(level);
+		  	    r = Temperatures[temp_index][0]*(brightness/255.0);
+            g = Temperatures[temp_index][1]*(brightness/255.0);
+            b = Temperatures[temp_index][2]*(brightness/255.0);
+            glow();
+	    	}
   
-        void Increase_Brightness(){
-            brightness_function_index = 1;
-            update();
-        }
-  
-        void Decrease_Brightness(){
-            brightness_function_index = 2;
-            update();
-        }
-  
-  		void Hotter(){
-          	if(temp_index < 10){
-            	temp_index++;
-          	}
-          	update();
-        }
-  
-        void Cooler(){
-          	if(temp_index > 0){
-              	temp_index--;
+        byte Hotter(){
+            if(temp_index < 10){
+                temp_index++;
             }
-          	update();
+            update();
+			      return 0;
+        }
+  
+        byte Cooler(){
+            if(temp_index > 0){
+                temp_index--;
+            }
+        update();
+			  return 0;
         }
 };
 
-char data[4];
-const int data_size = 4;
+byte data[5] = {1, 1, 0, 0, 7};
+const int data_size = 5;
 LED Light;
 
 
 void setup(){
-  Serial.begin(9600);
-  pinMode(LDR_PIN, INPUT);
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  
-  Light.brightness = DEFAULT_BRIGHTNESS;
-  Light.glow();
+    Serial.begin(9600);
+    Wire.begin();
+    lightInput.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
+    //pinMode(LUX_PIN, INPUT);
+    pinMode(3, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+
+    Light.brightness = DEFAULT_BRIGHTNESS;
+    LUX = lightInput.readLightLevel();
+    Light.glow();
 }
 
 void loop(){
-  LDR = analogRead(LDR_PIN);
-  if(Serial.available())
-  {
-    Serial.readBytes(data, data_size);
-    if(data[0]=='1')
-    {
+    lightInput.configure(BH1750::ONE_TIME_HIGH_RES_MODE);
+    LUX = lightInput.readLightLevel();
+    if(Serial.available())
+      Serial.readBytes(data, data_size);
+    if(data[0]==1){
       Light.glow();
-      if(data[1]=='0')
-      {
-        Light.glow();
-      }
-      if(data[1]=='1')
+      if(data[1]==0)
+        Light.update_with_const_brightness(int(data[4]));
+      if(data[1]==1)
         Light.update();
-      if(data[2]=='1')
-        Light.Increase_Brightness();
-      if(data[2]=='2')
-        Light.Decrease_Brightness();
-      if(data[3]=='1')
-        Light.Hotter();
-      if(data[3]=='2')
-        Light.Cooler();
+      if(data[3]==1)
+        data[3] = Light.Hotter();
+      if(data[3]==2)
+        data[3] = Light.Cooler();
     }
-    else if(data[0]=='0')
+    else if(data[0]==0)
       Light.dim();
-  }
+    Serial.println(100-Light.brightness/2.55);
+    delay(2000);
 }
